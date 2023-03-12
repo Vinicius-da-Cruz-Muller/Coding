794b7ed3f023dad73da99bfcdf85275d4c3a35a5
Vinicius-da-Cruz-Muller <108363297+Vinicius-da-Cruz-Muller@users.noreply.github.com>
2023-03-11 22:31:46 -0300

    Create aula2.py

---
+++ python/aula2.py
@@ -0,0 +1,13 @@
+print(12, 34, sep="-", end="\r\n") #sep é separador, end é quebra de linha
+print(56, 78, "Vinícius", sep='*', end="XXX")
+print(56, 78, "Vinícius", sep='*')
+
+
+# /r/n - CRLF, parte inferior do menu VSCode. Quebra de linha padrão do Windows.
+
+print("Vinícius \"Vini\" Muller") #\ caractere de escape, ignora o caractere seguinte.
+print(r"Vinícius \"Vini\" Muller") #r mostra os caracteres de escape.
+print('"Frase com aspa dupla"')
+print("'Frase com aspa simples'")
+
+# ctrl + ; comenta várias linhas selecionadas
