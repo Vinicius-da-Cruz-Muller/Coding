654df3ff3e41a4f12ae6a52ca9135122d4f4f06f
Vinicius-da-Cruz-Muller <108363297+Vinicius-da-Cruz-Muller@users.noreply.github.com>
2023-03-14 16:40:19 -0300

    14/03

---
+++ python/aula3.py
@@ -0,0 +1,13 @@
+nome = 'Tyrion Lannister'
+idade = 37
+altura = 1.27
+status = idade >= 18
+
+print(nome)
+print(idade)
+print(altura)
+print(status)
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(status))
