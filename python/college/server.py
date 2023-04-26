7026414b5353648c1c8ee42d5281c4cc89e6dd71
Vinicius-da-Cruz-Muller <108363297+Vinicius-da-Cruz-Muller@users.noreply.github.com>
2023-04-26 19:22:11 -0300

    exercício aula Joe

--- python/college/server.py
+++ python/college/server.py
@@ -1,6 +1,11 @@
 from xmlrpc.server import SimpleXMLRPCServer
 
 def is_even(n):
-    print("Requisição recebida com o seguinte argumento: ")
+    print("Requisição recebida com o seguinte argumento: " + str(n))
+    return n % 2 == 0
 
-#Continuar depois, com os slides no Teams
+server = SimpleXMLRPCServer(("localhost", 8000))
+print("Listening on port 8000...")
+server.register_function(is_even, "is_even")
+server.serve_forever()
+
