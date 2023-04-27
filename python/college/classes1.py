ae77c6f1164aeb90cbcfcce30de8dd988d4bc5c2
Vinicius-da-Cruz-Muller <108363297+Vinicius-da-Cruz-Muller@users.noreply.github.com>
2023-04-27 19:18:43 -0300

    Update classes1.py

--- python/college/classes1.py
+++ python/college/classes1.py
@@ -1,5 +1,5 @@
-class Usuario:
-    def __init__(self, nome, idade):
+class Usuario: #Palavra class diz ao interpretador que ali começa uma classe
+    def __init__(self, nome, idade): 
         self.nome = nome
         self.idade = idade
     def boas_vindas(self):
@@ -11,3 +11,10 @@ usuario1.boas_vindas()
 usuario2.boas_vindas()
 print(usuario1.nome)
 print(usuario2.idade)
+usuario1.nome = 'Tywin'
+print(usuario1.nome)
+
+
+"""A palavra __init__ (com dois ‘_’ em cada lado) define o construtor, que terá
+self como parâmetro (instância padrão), seguido dos parâmetros que
+criarmos, separados por vírgula"""
