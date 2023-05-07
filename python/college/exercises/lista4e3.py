4b7680b697b6d29f37b4c72296d739f6599267b7
Vinicius-da-Cruz-Muller <108363297+Vinicius-da-Cruz-Muller@users.noreply.github.com>
2023-05-07 19:30:13 -0300

    api rest estudos prova 9/5

---
+++ python/college/exercises/lista4e3.py
@@ -0,0 +1,7 @@
+"""3.Classe Retangulo:
+Crie uma classe que modele um retângulo:
+a.Atributos: Lado_a, Lado_b
+b.Métodos: Mudarvalordoslados, Retornarvalordoslados, calcularÁrea e calcularPerímetro;
+c.Crie um programa que utilize esta classe. Ele deve pedir ao usuário que informe 
+as medidas de um local. Depois, deve criar um objeto com as medidas ecalcular a quantidade de 
+pisos e de rodapés necessárias para o local."""
