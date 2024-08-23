122d233dfd6a766ee358bfb8ab288ed11f09353d
Vinicius-da-Cruz-Muller <108363297+Vinicius-da-Cruz-Muller@users.noreply.github.com>
2024-08-22 21:33:25 -0300

    início javascript

---
+++ front/Udemy - React/6_JS_Tipos de dados/js/scripts.js
@@ -0,0 +1,21 @@
+// 1 - Console, tipos e operadores
+console.log(typeof(2))
+console.log(typeof("teste"))
+console.log(5 * (4+12))
+console.log(typeof(NaN))
+console.log(typeof(Infinity))
+console.log(typeof(1==1))
+console.log(typeof(false))
+console.log(typeof(30 > 10))
+
+// Strings
+console.log("Um texto")
+console.log('Mais um texto')
+console.log(`E mais um texto`)
+
+// Símbolos especiais
+console.log("Texto escrito em \nduas linhas")
+
+// Concatenação e interpolação (template strings)
+console.log("Texto" + " mais texto")
+console.log(`A soma de dois mais dois é: ${2+2}`)
